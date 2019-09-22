ae9b3f34b20af24b792dbdd18faf9e34c4053c5b
pratajsk <p.ratajski@gmail.com>
2019-09-22 15:31:05 +0200

    Introduce menu to application

--- Kalkulator101/Program.cs
+++ Kalkulator101/Program.cs
@@ -10,6 +10,18 @@ namespace Kalkulator101
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Dodawanie");
+                Console.WriteLine("2. Odejmowanie");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 0)
+                {
+                    break;
+                }
+            }
+
         }
     }
 }
